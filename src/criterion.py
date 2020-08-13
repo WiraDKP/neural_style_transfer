af5cdede99231f42186ba2481984b1d726dfea63
Wira Dharma Kencana Putra <>
2020-08-14 02:12:50 +0700

    update criterion

--- src/criterion.py
+++ src/criterion.py
@@ -2,13 +2,15 @@ from torch.nn.functional import mse_loss
 from src.utils import gram_matrix
 
 
-def criterion(content_features, style_features, output_features, content_weight=1, style_weight=1e5):
+def criterion(content_features, style_features, output_contents, output_styles, content_weight=1, style_weight=1e6):
     # Content Loss
-    content_loss = mse_loss(content_features[0], output_features[-1])
+    content_loss = 0
+    for c, o in zip(content_features, output_contents):
+        content_loss += mse_loss(c, o)
     
     # Style Loss
     style_loss = 0
-    for s, o in zip(style_features, output_features):
+    for s, o in zip(style_features, output_styles):
         style_texture = gram_matrix(s)
         output_texture = gram_matrix(o)
         style_loss += mse_loss(style_texture, output_texture)
